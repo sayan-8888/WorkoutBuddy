7dd8e324d08b9f92684b99e3a2d4279f7c3cb9bc
Rishav <rishavpramanik17@gmail.com>
2024-08-30 04:44:50 +0530

    added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,40 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-mongoose.set("strictQuery", false)
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); // Import CORS
+const workoutRoutes = require("./routes/workouts");
+mongoose.set("strictQuery", false);
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(
+  cors({
+   origin: "https://workoutbuddy-frontend-mv6w.onrender.com", // Replace with your actual frontend URL
+  })
+);
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
 const port = process.env.PORT || 5000;
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
+    console.log("connected to database");
     // listen to port
-    app.listen(port, () => console.log("listening for requests on  ${port}!"));
+    app.listen(port, () => console.log(`listening for requests on ${port}!`));
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.log(err);
+  });
