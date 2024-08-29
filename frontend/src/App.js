3c2b96f91d03eef7600baad81a43bf27e520b249
Rishav <rishavpramanik17@gmail.com>
2024-08-30 03:53:22 +0530

    error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,9 +12,9 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
+            <Route
+              path="https://workoutbuddy-backend-au73.onrender.com/"
+              element={<Home />}
             />
           </Routes>
         </div>
